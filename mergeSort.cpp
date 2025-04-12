e3158a83bf0d20c86b21d6dced4b441734f20b6d
Arun9339 <kumararun94399@gmai.com>
2025-04-12 19:07:21 +0530

    Merge sort code

---
+++ mergeSort.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int  main(){
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    for(int i=0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
