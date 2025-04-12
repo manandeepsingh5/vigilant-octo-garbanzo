67e62ee0425ebe240fedcc72d3940c0e35574682
Kartik <kartik.mathur10@gmail.com>
2025-04-12 18:24:48 +0530

    Adding BubbleSort.cpp for code of sorting using bubble algo

---
+++ BubbleSort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {6,5,3,7,2,1};
+    int n = sizeof(arr)/sizeof(int);
+
+    // Missing bubbleSort code ????
+
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i] << " ";
+    }
+    
+    cout << endl;
+}
