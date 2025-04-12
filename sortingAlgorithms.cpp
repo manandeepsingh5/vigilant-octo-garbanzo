354fa100d3380f9c7886eb7031c727ef38caf7e3
Kartik <kartik.mathur10@gmail.com>
2025-04-12 17:38:45 +0530

    Creatin an array file, where we can use different sorting algorithms

---
+++ sortingAlgorithms.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[] = {6,5,3,7,2,1};
+    int n = sizeof(arr)/sizeof(int);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i] << " ";
+    }
+    
+    cout << endl;
+}
